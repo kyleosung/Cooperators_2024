bef937d3ffb4a8478f0551e9ca2b4877770bd03d
Kyle Sung <kyleosung@gmail.com>
2024-02-23 14:07:13 -0500

    models

--- solution_library.py
+++ solution_library.py
@@ -19,6 +19,8 @@ from sklearn import svm
 from sklearn.impute import SimpleImputer
 
 
+## ------------------------------------------------------
+## Functions Below
 
 def RMSE(x, y):
     MSE = ((y - x) ** 2).mean()
@@ -27,6 +29,39 @@ def RMSE(x, y):
 
 def load_claims():
     return pd.read_csv("./Qualification_Package/Claims_Years_1_to_3.csv")
+
+
+def claims_preprocess_get_xy():
+    claims = pd.read_csv("./Qualification_Package/Claims_Years_1_to_3.csv")
+    claims['pol_pay_freq'] = claims['pol_pay_freq'].replace( {'Biannual': 2, 'Yearly': 1, 'Monthly': 12, 'Quarterly': 4} )
+    claims['pol_payd'] = claims['pol_payd'].replace( {'No': 0, 'Yes': 1} )
+    claims['drv_sex1'] = claims['drv_sex1'].replace( {'M': 1, 'F': 0} )
+    claims['vh_type'] = claims['vh_type'].replace( {'Tourism': 1, 'Commercial': 0} )
+    claims['drv_drv2'] = claims['drv_drv2'].replace( {'No': 0, 'Yes': 1} )
+
+    claims['vh_make_model'] = claims['vh_make_model'].apply(hash)
+
+    objects = claims.select_dtypes(['object'])
+    categorical = claims.select_dtypes(['int64'])
+    continuous = claims.select_dtypes(['float64'])
+
+    objects_filled = SimpleImputer(missing_values=np.nan, strategy='most_frequent').fit_transform(objects)
+    categorical_filled = SimpleImputer(missing_values=np.nan, strategy='median').fit_transform(categorical)
+    continuous_filled = SimpleImputer(missing_values=np.nan, strategy='mean').fit_transform(continuous)
+
+    objects_filled = pd.DataFrame(objects_filled, columns=objects.columns.to_list())
+    categorical_filled = pd.DataFrame(categorical_filled, columns=categorical.columns.to_list())
+    continuous_filled = pd.DataFrame(continuous_filled, columns=continuous.columns.to_list())
+    
+    design_matrix = pd.get_dummies(objects_filled, columns=['pol_usage', 'drv_sex2', 'vh_fuel'], dtype=int)
+    objects_design = design_matrix.drop(columns=['id_policy'])
+
+    df = pd.concat([objects_design, categorical_filled, continuous_filled], axis = 1)
+
+    x = df.iloc[:,:-1]
+    y = df.iloc[:,-1]
+    return x, y
+
 
 def load_tests():
     return pd.read_csv("./Qualification_Package/Submission_Data.csv")
@@ -106,11 +141,13 @@ def preprocess_x(dataframe):
     return df
 
 
-def evaluate(claims, model):
+def evaluate_scratch(claims, model):
     x_train, x_test, y_train, y_test = preprocess_xy(claims)
     model.fit(x_train, y_train)
     y_pred = model.predict(x_test)
     return np.sqrt(metrics.mean_squared_error(y_test, y_pred))
+
+
 
 def predict(x, model):
     x_new = preprocess_x(x)
